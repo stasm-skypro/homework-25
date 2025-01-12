ba267e54edde074af105ce221750e68a85779df4
Станислав Маяцкий <stanislavmayatsky@192.168.10.11>
2025-01-12 20:43:49 +0500

    FBV for product_details view changed on to CBV

--- catalog/views.py
+++ catalog/views.py
@@ -5,7 +5,7 @@
 from django.shortcuts import render
 from catalog.models import Product
 from catalog.forms import ContactForm, ProductForm
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 
 class ProductListView(ListView):
@@ -13,9 +13,20 @@ class ProductListView(ListView):
     Определяет отображение страницы home - product_list.html.
     """
     model = Product
+    # По правилам CBV имя шаблона нужно задать как catalog/product_list. Тогда можно было бы обойтись без переменных
+    # template_name и context_object_name. Для реализации такого подхода нужно создать в папке templates подкаталог
+    # catalog, переместить все шаблоны в этот подкаталог и скорректировать пути внутри шаблонов.
     template_name = "product_list.html"
     context_object_name = "home"
 
+
+class ProductDetailView(DetailView):
+    """
+    Определяет отображение детализации (характеристик) продукта.
+    """
+    model = Product
+    template_name = "product_details.html"
+    context_object_name = "product_details"
 
 # def home_view(request):
 #     """
@@ -23,6 +34,14 @@ class ProductListView(ListView):
 #     """
 #     products_list = Product.objects.all().order_by("id")
 #     return render(request, "catalog/home.html", context={"products_list": products_list})
+
+
+# def product_details_view(request, product_id: int = 1):
+#     """
+#     Определяет отображение детализации (характеристик) продукта.
+#     """
+#     product = Product.objects.get(id=product_id)
+#     return render(request, "product_details.html", context={"product": product})
 
 
 def contacts_view(request):
@@ -37,14 +56,6 @@ def contacts_view(request):
     else:
         form = ContactForm()
     return render(request, "contacts.html", {"form": form})
-
-
-def product_details_view(request, product_id: int = 1):
-    """
-    Определяет отображение детализации (характеристик) продукта.
-    """
-    product = Product.objects.get(id=product_id)
-    return render(request, "product_details.html", context={"product": product})
 
 
 def add_product_view(request):
