01a42e493293c961d29f7de6ce043833223bd647
Станислав Маяцкий <stanislavmayatsky@192.168.10.11>
2025-01-13 22:10:40 +0500

    implemented create form

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,16 +4,13 @@ URL configuration for app catalog configuring.
 
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import contacts_view, ProductDetailView, ProductListView, add_product_view
+from catalog.views import ProductDetailView, ProductListView, ProductCreateView
 
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path("", ProductListView.as_view(), name="product_list.html"),
-    path("product_details/<int:pk>", ProductDetailView.as_view(), name="product_details.html"),
-    # path("", home_view, name="home.html"),
-    # path("product_details/<int:product_id>", product_details_view, name="product_details.html"),
-    path("contacts/", contacts_view, name="contacts.html"),
-    path("add_product/", add_product_view, name="add_product.html"),
+    path("", ProductListView.as_view(), name="product_list"),
+    path("product_detail/<int:pk>", ProductDetailView.as_view(), name="product_detail"),
+    path("product_form/", ProductCreateView.as_view(), name="product_create"),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,23 +1,23 @@
 """
 Контроллеры, определённые внутри приложения catalog.
 """
+from django.urls import reverse_lazy
 
-from django.shortcuts import render
 from catalog.models import Product
-from catalog.forms import ContactForm, ProductForm
-from django.views.generic import ListView, DetailView
+# from catalog.forms import ProductForm
+from django.views.generic import ListView, DetailView, CreateView
 
 
 class ProductListView(ListView):
     """
-    Определяет отображение страницы home - product_list.html.
+    Определяет отображение страницы product_list.html.
     """
     model = Product
     # По правилам CBV имя шаблона нужно задать как catalog/product_list. Тогда можно было бы обойтись без переменных
     # template_name и context_object_name. Для реализации такого подхода нужно создать в папке templates подкаталог
     # catalog, переместить все шаблоны в этот подкаталог и скорректировать пути внутри шаблонов.
-    template_name = "product_list.html"
-    context_object_name = "home"
+    # template_name = "product_list.html"
+    # context_object_name = "product_list"
 
 
 class ProductDetailView(DetailView):
@@ -25,8 +25,37 @@ class ProductDetailView(DetailView):
     Определяет отображение детализации (характеристик) продукта.
     """
     model = Product
-    template_name = "product_details.html"
-    context_object_name = "product_details"
+    # template_name = "product_detail.html"
+    # context_object_name = "product_detail"
+
+
+class ProductCreateView(CreateView):
+    """
+    Определяет отображение добавления продукта.
+    """
+    model = Product
+    fields = ["product", "description", "image", "category", "price", "created_at", "changed_at"]
+    # template_name = "product_form"
+    # context_object_name = "product_create"
+    success_url = reverse_lazy("catalog:product_list")
+
+    def form_valid(self, form):
+        """
+        Дополнительная обработка перед сохранением формы.
+        """
+        self.object = form.save()  # Сохраняем объект формы в базу
+        print("Форма прошла валидацию")
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        """
+        Обработка в случае неверной формы.
+        """
+        print("Форма не прошла валидацию")
+        print(form.errors)  # Вывод ошибок формы в консоль
+        return super().form_invalid(form)
+
+
 
 # def home_view(request):
 #     """
@@ -44,29 +73,29 @@ class ProductDetailView(DetailView):
 #     return render(request, "product_details.html", context={"product": product})
 
 
-def contacts_view(request):
-    """
-    Определяет отображение страницы contacts.html.
-    """
-    if request.method == "POST":
-        form = ContactForm(request.POST)
-        if form.is_valid():
-            form.save()  # Сохраняем данные в таблицу catalog_contacts
-            return render(request, "success.html")
-    else:
-        form = ContactForm()
-    return render(request, "contacts.html", {"form": form})
+# def add_product_view(request):
+#     """
+#     Определяет отображение добавления продукта.
+#     """
+#     if request.method == "POST":
+#         form = ProductForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             form.save()  # Сохраняем данные в таблицу catalog_products
+#             return render(request, "success.html")
+#     else:
+#         form = ProductForm()
+#     return render(request, "add_product.html", {"form": form})
 
 
-def add_product_view(request):
-    """
-    Определяет отображение добавления продукта.
-    """
-    if request.method == "POST":
-        form = ProductForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()  # Сохраняем данные в таблицу catalog_products
-            return render(request, "success.html")
-    else:
-        form = ProductForm()
-    return render(request, "add_product.html", {"form": form})
+# def contacts_view(request):
+#     """
+#     Определяет отображение страницы contacts.html.
+#     """
+#     if request.method == "POST":
+#         form = ContactForm(request.POST)
+#         if form.is_valid():
+#             form.save()  # Сохраняем данные в таблицу catalog_contacts
+#             return render(request, "success.html")
+#     else:
+#         form = ContactForm()
+#     return render(request, "contacts.html", {"form": form})
